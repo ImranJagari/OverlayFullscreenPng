6171594a128663814edf4a24c43f78ada10f944d
Imran J <imranjagari@hotmail.com>
2020-12-23 23:20:16 +0100

    update assemblies infos

--- OverlayFullscreenPng/Properties/AssemblyInfo.cs
+++ OverlayFullscreenPng/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // Les informations générales relatives à un assembly dépendent de
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("OverlayFullscreenPng")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Scanlines For Windows ©")]
+[assembly: AssemblyDescription("Show scanlines as topmost windows by applying background image on fullscreen window")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OverlayFullscreenPng")]
+[assembly: AssemblyProduct("Scanlines For Windows ©")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +33,6 @@ using System.Runtime.InteropServices;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.3.0")]
+[assembly: AssemblyFileVersion("0.2.3.0")]
+[assembly: NeutralResourcesLanguage("fr")]
